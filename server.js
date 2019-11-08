03d054da9ee8d216091862e6a555337e8a519189
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-11-08 10:48:16 -0600

    created server

---
+++ server.js
@@ -0,0 +1,23 @@
+//imports and dependencies
+const express = require('express');
+const db = require('/data/dbConfig');
+const projectsRouter = require('./projects/projectsRouter');
+const resourcesRouter = require('./projects/resourcesRouter')
+
+
+//server
+const server = express();
+
+//middleware
+server.use(express.json());
+server.use('/api/projects', projectsRouter);
+server.use('/api/resources', resourcesRouter )
+
+//test
+server.get('/', (req, res) =>{
+    res.send('testing')
+} );
+
+//export
+
+module.exports = server;
